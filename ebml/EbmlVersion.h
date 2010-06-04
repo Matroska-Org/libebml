1fb7b2d678806b9269fb9ac247b9093da4e1b712
Moritz Bunkus <moritz@bunkus.org>
2010-06-04 08:42:42 +0000

    Set version number to 1.0.0
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/libebml@291 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- ebml/EbmlVersion.h
+++ ebml/EbmlVersion.h
@@ -11,12 +11,12 @@
 ** modify it under the terms of the GNU Lesser General Public
 ** License as published by the Free Software Foundation; either
 ** version 2.1 of the License, or (at your option) any later version.
-** 
+**
 ** This library is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ** Lesser General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU Lesser General Public
 ** License along with this library; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -42,9 +42,9 @@
 
 START_LIBEBML_NAMESPACE
 
-#define LIBEBML_VERSION 0x000801
+#define LIBEBML_VERSION 0x010000
 
-static const std::string EbmlCodeVersion = "0.8.1";
+static const std::string EbmlCodeVersion = "1.0.0";
 static const std::string EbmlCodeDate    = __TIMESTAMP__;
 
 /*!
