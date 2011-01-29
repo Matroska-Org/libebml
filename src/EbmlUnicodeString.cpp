4ced4ea53efff89686f4af96881848eb3aa29484
Moritz Bunkus <moritz@bunkus.org>
2011-01-29 11:42:15 +0000

    Fix handling of UTF-8 strings with characters encoded in more than three bytes.
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/libebml@647 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- src/EbmlUnicodeString.cpp
+++ src/EbmlUnicodeString.cpp
@@ -137,27 +137,38 @@ void UTFstring::UpdateFromUTF8()
 	// find the size of the final UCS-2 string
 	size_t i;
 	for (_Length=0, i=0; i<UTF8string.length(); _Length++) {
-		if ((UTF8string[i] & 0x80) == 0) {
+		uint8 lead = static_cast<uint8>(UTF8string[i]);
+		if (lead < 0x80)
 			i++;
-		} else if ((UTF8string[i] & 0x20) == 0) {
+		else if ((lead >> 5) == 0x6)
 			i += 2;
-		} else if ((UTF8string[i] & 0x10) == 0) {
+		else if ((lead >> 4) == 0xe)
 			i += 3;
-		}
+		else if ((lead >> 3) == 0x1e)
+			i += 4;
+		else
+			// Invalid size?
+			break;
 	}
 	_Data = new wchar_t[_Length+1];
 	size_t j;
 	for (j=0, i=0; i<UTF8string.length(); j++) {
-		if ((UTF8string[i] & 0x80) == 0) {
-			_Data[j] = UTF8string[i];
+		uint8 lead = static_cast<uint8>(UTF8string[i]);
+		if (lead < 0x80) {
+			_Data[j] = lead;
 			i++;
-		} else if ((UTF8string[i] & 0x20) == 0) {
-			_Data[j] = ((UTF8string[i] & 0x1F) << 6) + (UTF8string[i+1] & 0x3F);
+		} else if ((lead >> 5) == 0x6) {
+			_Data[j] = ((lead & 0x1F) << 6) + (UTF8string[i+1] & 0x3F);
 			i += 2;
-		} else if ((UTF8string[i] & 0x10) == 0) {
-			_Data[j] = ((UTF8string[i] & 0x0F) << 12) + ((UTF8string[i+1] & 0x3F) << 6) + (UTF8string[i+2] & 0x3F);
+		} else if ((lead >> 4) == 0xe) {
+			_Data[j] = ((lead & 0x0F) << 12) + ((UTF8string[i+1] & 0x3F) << 6) + (UTF8string[i+2] & 0x3F);
 			i += 3;
-		}
+		} else if ((lead >> 3) == 0x1e) {
+			_Data[j] = ((lead & 0x07) << 18) + ((UTF8string[i+1] & 0x3F) << 12) + ((UTF8string[i+2] & 0x3F) << 6) + (UTF8string[i+3] & 0x3F);
+			i += 4;
+		} else
+			// Invalid char?
+			break;
 	}
 	_Data[j] = 0;
 }
