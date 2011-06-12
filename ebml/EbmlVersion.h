83cc857424537cabf419f825211166344404deca
Steve Lhomme <slhomme@matroska.org>
2011-06-12 09:21:05 +0000

    update the version headers
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/libebml@756 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- ebml/EbmlVersion.h
+++ ebml/EbmlVersion.h
@@ -42,9 +42,9 @@
 
 START_LIBEBML_NAMESPACE
 
-#define LIBEBML_VERSION 0x010200
+#define LIBEBML_VERSION 0x010201
 
-static const std::string EbmlCodeVersion = "1.2.0";
+static const std::string EbmlCodeVersion = "1.2.1";
 static const std::string EbmlCodeDate    = __TIMESTAMP__;
 
 /*!
