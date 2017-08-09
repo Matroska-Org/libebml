f060b0394d7383ed245176b62823be04c1bbbace
Moritz Bunkus <moritz@bunkus.org>
2017-08-09 19:48:26 +0200

    bump version number to 1.3.5

--- ebml/EbmlVersion.h
+++ ebml/EbmlVersion.h
@@ -42,7 +42,7 @@
 
 START_LIBEBML_NAMESPACE
 
-#define LIBEBML_VERSION 0x010304
+#define LIBEBML_VERSION 0x010305
 
 extern const std::string EbmlCodeVersion;
 extern const std::string EbmlCodeDate;
