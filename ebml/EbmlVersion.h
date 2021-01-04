f9fc9a89177e9bcdc1a34ac7ab76114ddd6149c8
Moritz Bunkus <moritz@bunkus.org>
2021-01-04 15:18:57 +0100

    bump version number to 1.4.1, add release message

--- ebml/EbmlVersion.h
+++ ebml/EbmlVersion.h
@@ -42,7 +42,7 @@
 
 START_LIBEBML_NAMESPACE
 
-#define LIBEBML_VERSION 0x010400
+#define LIBEBML_VERSION 0x010401
 
 extern const EBML_DLL_API std::string EbmlCodeVersion;
 extern const EBML_DLL_API std::string EbmlCodeDate;
