41ef61531eb6181d329da279ae6f0930b670476a
Moritz Bunkus <mo@bunkus.online>
2022-09-24 21:55:46 +0200

    bump version number to 1.4.3

--- ebml/EbmlVersion.h
+++ ebml/EbmlVersion.h
@@ -42,7 +42,7 @@
 
 namespace libebml {
 
-#define LIBEBML_VERSION 0x010402
+#define LIBEBML_VERSION 0x010403
 
 extern const EBML_DLL_API std::string EbmlCodeVersion;
 extern const EBML_DLL_API std::string EbmlCodeDate;
