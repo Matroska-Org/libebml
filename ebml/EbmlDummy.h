84413763fdc0d4e66590ea2cf56f3c46c9cac5fe
Steve Lhomme <slhomme@matroska.org>
2024-01-01 11:20:39 +0100

    EbmlDummy: remove overriden methods that have the default behavior

--- ebml/EbmlDummy.h
+++ ebml/EbmlDummy.h
@@ -23,8 +23,6 @@ class EBML_DLL_API EbmlDummy : public EbmlBinary {
       return DummyId;
     }
 
-    const EbmlSemanticContext &Context() const override;
-    const char *DebugName() const override { return "DummyElement"; }
     EbmlElement & CreateElement() const override { return Create(); }
     EbmlElement * Clone() const override { return new EbmlDummy(DummyId); }
 
