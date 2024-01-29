ffc640fad638c698904bc234af460de73f239fcb
Rosen Penev <rosenp@gmail.com>
2024-01-28 17:35:18 -0800

    fix bool return
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- ebml/EbmlElement.h
+++ ebml/EbmlElement.h
@@ -416,7 +416,7 @@ class EBML_DLL_API EbmlSemanticContext {
 
     bool operator!=(const EbmlSemanticContext & aElt) const {
       return ((Size != aElt.Size) || (MyTable != aElt.MyTable) ||
-        (UpTable != aElt.UpTable) || (GetGlobalContext != aElt.GetGlobalContext) |
+        (UpTable != aElt.UpTable) || (GetGlobalContext != aElt.GetGlobalContext) ||
         (MasterElt != aElt.MasterElt));
     }
 
