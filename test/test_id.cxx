11a86abc92ba358fdefc425cdc2d8a7cab817eec
Rosen Penev <rosenp@gmail.com>
2024-02-17 15:12:34 -0800

    remove unused code
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- test/test_id.cxx
+++ test/test_id.cxx
@@ -14,9 +14,6 @@ public:
 
 int main(void)
 {
-    constexpr binary buf[4] = { 0x12, 0x34, 0x56, 0x78 };
-    EbmlId frombuf(EbmlId::FromBuffer(buf, 4));
-
     EbmlId fromint(0x12345678);
 
     EbmlId copy(fromint);
