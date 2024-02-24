3ccbde5d560938b21d1c36f696476638c1bead97
Steve Lhomme <slhomme@matroska.org>
2024-02-24 10:50:58 +0100

    make some EbmlElement inlines return constexpr objects
    Even though there's no constexpr constructor.

--- ebml/EbmlElement.h
+++ ebml/EbmlElement.h
@@ -482,8 +482,8 @@ class EBML_DLL_API EbmlElement {
 
     virtual EbmlId const &GetClassId() const {return ClassInfo.ClassId();}
     virtual explicit operator const EbmlId &() const { return GetClassId(); }
-    const char *DebugName() const {return ClassInfo.GetName();}
-    const EbmlSemanticContext &Context() const {return ClassInfo.GetContext();}
+    constexpr const char *DebugName() const {return ClassInfo.GetName();}
+    constexpr const EbmlSemanticContext &Context() const {return ClassInfo.GetContext();}
         virtual EbmlElement & CreateElement() const = 0;
 
     /*!
