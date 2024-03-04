8de047e0d132c61499f2c21d57c8a40942d8f225
Steve Lhomme <slhomme@matroska.org>
2024-03-04 07:21:49 +0100

    remove constexpr from DEFINE_xxx_CONTEXT()
    It prevents from using DLL export (in libmatroska). We can set it locally when needed.

--- src/EbmlContexts.cpp
+++ src/EbmlContexts.cpp
@@ -16,7 +16,7 @@ DEFINE_SEMANTIC_ITEM(false, false, EbmlCrc32)
 DEFINE_SEMANTIC_ITEM(false, false, EbmlVoid)
 DEFINE_END_SEMANTIC(EbmlGlobal)
 
-static DEFINE_xxx_CONTEXT(EbmlGlobal, GetEbmlGlobal_Context)
+static constexpr DEFINE_xxx_CONTEXT(EbmlGlobal, GetEbmlGlobal_Context)
 
 const EbmlSemanticContextMaster & GetEbmlGlobal_Context()
 {
